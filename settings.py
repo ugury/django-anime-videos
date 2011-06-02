4eb00c01303a30cb5e511ad66c5794bf7710c545
ugury <uguryoruk21@gmail.com>
2011-06-02 03:24:02 -0700

    Edited settings.py via GitHub

--- settings.py
+++ settings.py
@@ -13,9 +13,9 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'luterien_shin',                      # Or path to database file if using sqlite3.
-        'USER': 'luterien_shin',                      # Not used with sqlite3.
-        'PASSWORD': '53152188',                  # Not used with sqlite3.
+        'NAME': '',                      # Or path to database file if using sqlite3.
+        'USER': '',                      # Not used with sqlite3.
+        'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
     }
